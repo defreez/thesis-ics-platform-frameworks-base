c8ef73bbf0305015d2a935a278ee3a06e5be2c49
Daniel DeFreez <daniel@defreez.com>
2012-02-17 09:54:59 -0800

    kill colornote as demo

--- services/java/com/android/server/MountService.java
+++ services/java/com/android/server/MountService.java
@@ -22,6 +22,7 @@ import com.android.server.am.ActivityManagerService;
 import com.android.server.pm.PackageManagerService;
 
 import android.Manifest;
+import android.app.ActivityManager;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
@@ -541,9 +542,12 @@ class MountService extends IMountService.Stub
                     String state = SystemProperties.get("ro.crypto.state");
                     if (state.equals("encrypted"))  {
                         Slog.d(TAG, "Received screen off broadcast, and encrypted, time to wipe keys.");
+			ActivityManager mAm;
+			mAm = (ActivityManager) mContext.getSystemService(Context.ACTIVITY_SERVICE);
                         mConnector.doCommand("cryptfs clearmaster");
 			// This isn't terribly useful - use callout to logic for what apps to secure
                         mConnector.doCommand("cryptfs clearboundary 10036");
+			mAm.forceStopPackage("com.socialnmobile.dictapps.notepad.color.note");
                     }
             }
         }
